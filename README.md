269312cc44d9516a86f9d00003093f60eb0f8309
lzlniu <lzlniu@qq.com>
2020-03-17 00:18:01 +0800

    update gbtree.m

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # m_related
-## 1 gbfileget.m
+## 1 gbtree.m
 
-gbfileget.m is a MATLAB script which can get multiple .gb files from a list.txt, list.txt should be placed in the same dictionary with gbfileget.m, The list.txt should be printed only one row, each line is a NCBI accession number. It can be used in Windows or Linux, just using MATLAB desktop software. Or you can use in Linux Shell by -- "matlab -nodisplay -r gbfileget". Then you may type "exit()" to exit MATLAB command line.
+gbtree.m is a MATLAB script which can get multiple .gb files from a list.txt, list.txt should be placed in the same dictionary with gbfileget.m, The list.txt should be printed only one row, each line is a NCBI accession number. It can be used in Windows or Linux, just using MATLAB desktop software. Or you can use it in Linux Shell by -- "matlab -nodisplay -r gbtree". Then you may type "exit()" to exit MATLAB command line.
 
 ## 2 gbaa.sh
 
--- gbtree.m
+++ gbtree.m
@@ -1,29 +1,27 @@
 %author:lizelin
 %date:2020/03/15
 %utility:automatically download multiple .gb file and sequence from genbank and draw a phylotree.
-%need:list.txt
-%output:tree.png (phylotree), multi_seqs.fa (multiple sequences in fasta
-%format), ACCNUM.gb (multiple .gb files, named as accession number)
+%need:list.txt (contain multiple accession number in one row)
+%output:tree.png (phylotree), multi_seqs.fa (multiple sequences in fasta format), many acc.gb (.gb files, named as accession number) in gbfiles (dictionary).
 clear
 clc
 options = weboptions;
-options.Timeout = 25;
+options.Timeout = 60;
 list = importdata('list.txt');
-len = length(list);
-parfor i = 1:len
+parfor i = 1:length(list)
     acc = char(list(i,1));
     gbname = [num2str(i),'_',acc,'.gb'];
     getgenbank(acc,'FILEFORMAT','Genbank','TOFILE',gbname);
     multi_seqs(i).Sequence = getgenbank(acc,'SEQUENCEONLY','true');
     multi_seqs(i).Header = acc;
 end
-clear len; clear list;
+mkdir gbfiles
+movefile *.gb gbfiles\
 fastawrite('multi_seqs.fa',multi_seqs);
 multi_seqs = fastaread('multi_seqs.fa');
 aligned_seqs = multialign(multi_seqs);
 distence = seqpdist(aligned_seqs);
 tree = seqneighjoin(distence,'equivar',aligned_seqs);
-clear multi_seqs; clear aligned_seqs; clear distence;
 plot(tree);
 grid;
 saveas(gcf,['./','tree.png']);
